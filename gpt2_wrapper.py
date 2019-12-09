b69986dcb69d0ac28efb7d3e2201687936cb72af
Nathan Dahlberg <nadahlberg@gmail.com>
2019-12-09 12:49:30 -0500

    Update gpt2_wrapper.py

--- gpt2_wrapper.py
+++ gpt2_wrapper.py
@@ -8,7 +8,7 @@ def check_model(c):
   if not os.path.isdir(os.path.join("models", c['model_name'])):
 	  gpt2.download_gpt2(model_name=c['model_name'])
 
-def gen(c, prefix=None):
+def generate(c, prefix=None):
   check_model(c)
   sess = gpt2.start_tf_sess()
   gpt2.load_gpt2(sess, model_name=c['model_name'])
@@ -17,6 +17,6 @@ def gen(c, prefix=None):
       length=c['output_length'],
       return_as_list=True,
   )
-  if c['prefix'] is not None:
+  if prefix is not None:
     args['prefix'] = prefix
   return gpt2.generate(sess, **args)[0]
