8b91254e87782d94c83b0defb8c9402f6d7238b9
Nathan Dahlberg <nadahlberg@gmail.com>
2019-12-09 14:27:48 -0500

    Update quick_gpt2.py

--- quick_gpt2.py
+++ quick_gpt2.py
@@ -1,22 +1,39 @@
 import gpt_2_simple as gpt2
+import requests
 import os
 
 class GPT2:
-	def __init__(self, base_model, custom=None):
+	def __init__(self, name, base_model):
+		self.name = name
 		self.base_model = base_model
-		self.custom = custom
-
+		if not os.path.exists('checkpoint'):
+			os.mkdir('checkpoint')
 
 	def check_model(self):
 		base_model = self.base_model
 		if not os.path.isdir(os.path.join("models", base_model)):
 			gpt2.download_gpt2(model_name=base_model)
 
-	def generate(self, length=1023, prefix=None):
+	def get_data(self, url):
+		with open('data.txt', 'w') as f:
+			r = requests.get(url)
+			f.write(r.content)
+
+	def finetune(self, steps):
 		base_model = self.base_model
-		self.check_model()
 		sess = gpt2.start_tf_sess()
-		gpt2.load_gpt2(sess, model_name=base_model)
+		gpt2.finetune(
+			sess,
+			'data.txt',
+			model_name=self.base_model,
+			run_name=self.name,
+			steps=steps,
+		) 
+
+	def generate(self, prefix=None, length=1023):
+		base_model = self.base_model
+		name = self.name
+		self.check_model()
 		args = dict(
 				model_name=base_model, 
 				length=length,
@@ -24,9 +41,13 @@ class GPT2:
 		)
 		if prefix is not None:
 			args['prefix'] = prefix
+		if not os.path.exists('checkpoint/' + name):
+			gpt2.load_gpt2(sess, model_name=base_model)
+		else:
+			args['run_name'] = name
+			gpt2.load_gpt2(sess, model_name=base_model, run_name=name)
+		sess = gpt2.start_tf_sess()
+		gpt2.load_gpt2(sess, model_name=base_model)
 		return gpt2.generate(sess, **args)[0]
-
-	def finetune(self):
-		pass
 
 
