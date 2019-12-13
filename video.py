bffb5087c395d1e3ca8779e93e10ec48d93119cb
Nathan Dahlberg <nadahlberg@gmail.com>
2019-12-13 16:51:01 -0500

    Update video.py

--- video.py
+++ video.py
@@ -1,11 +1,24 @@
+import numpy as np
 import cv2
+from google.colab.patches import cv2_imshow
 
 class VideoEditor:
-	def __init__(self, path):
-		self.path = path
-
-	def process(self, f):
+	def __init__(self):
 		pass
+
+	def process(self, f, in_path, out_path):
+		stream = cv2.VideoCapture(in_path)
+		fourcc = cv2.cv.CV_FOURCC(*'XVID')
+		out = cv2.VideoWriter(out_path, fourcc, 20.0, (640,480))
+
+		while 1:
+			ret, img = stream.read()
+			if ret:
+				img = f(img)
+				out.write(img)
+				cv2_imshow(img)
+			else:
+				break
 
 
 
