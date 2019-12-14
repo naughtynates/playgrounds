200ded9f9fcde2e4d2d076f5e9bcdc0d0c039eca
Nathan Dahlberg <nadahlberg@gmail.com>
2019-12-14 17:04:08 -0500

    update

--- video.py
+++ video.py
@@ -9,15 +9,15 @@ class VideoEditor:
 	def process(self, in_path, out_path):
 		stream = cv2.VideoCapture(in_path)
 		ret, img = stream.read()
-		img = self.f(img, frame_num)
+		img = self.f(img, 0)
 		fourcc = cv2.VideoWriter_fourcc(*'MP4V')
 		out = cv2.VideoWriter(out_path, fourcc, 20.0, (img.shape[1], img.shape[0]))
 		#out = cv2.VideoWriter(out_path, -1, 20.0, (640,480))
-		count = 0
+		frame_num = 0
 		while 1:
 			ret, img = stream.read()
-			count += 1
-			print('Frame:', count)
+			frame_num += 1
+			print('Frame:', frame_num)
 			if not ret or cv2.waitKey(25) & 0XFF == ord('q'):
 				cv2.destroyAllWindows()
 				break
