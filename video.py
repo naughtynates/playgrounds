442c6ed92e2ca09d5d7d7a40d151bd1303816095
Nathan Dahlberg <nadahlberg@gmail.com>
2019-12-13 19:35:08 -0500

    Update video.py

--- video.py
+++ video.py
@@ -8,8 +8,9 @@ class VideoEditor:
 
 	def process(self, f, in_path, out_path):
 		stream = cv2.VideoCapture(in_path)
+		ret, img = stream.read()
 		fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-		out = cv2.VideoWriter(out_path, fourcc, 20.0, (640,480))
+		out = cv2.VideoWriter(out_path, fourcc, 20.0, (img.shape[1], img.shape[0]))
 		#out = cv2.VideoWriter(out_path, -1, 20.0, (640,480))
 		count = 0
 		while 1:
